167ed94a169d22e349edf0d294357c6b33c38041
Naod <chris_naod@yahoo.com>
2023-06-01 21:41:36 +1000

    changed the schema model

--- backend/model/Contact.js
+++ backend/model/Contact.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 
 const contactSchema = new mongoose.Schema(
   {
-    firstName: { type: String, required: true },
+    firstName: { type: String, required: false },
     lastName: { type: String, required: false },
-    phoneNumber: { type: String, required: true },
+    phoneNumber: { type: String, required: false },
     email: { type: String, required: false },
   },
   { timestamps: true }
